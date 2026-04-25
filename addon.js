142a3e412d43b336458b3659531b0e7e79b28cf6
(ᇴ‿ฺᇴ) <spam-born@inbox.lv>
2026-04-25 19:24:53 +0200

    Update Description

--- addon.js
+++ addon.js
@@ -27,7 +27,7 @@ const manifest = {
     version: "9.3.0", 
     name: "Yomi",
     logo: BASE_URL + "/yomi.png", 
-    description: "The ultimate Debrid-powered Sukebei gateway.",
+    description: "The ultimate Sukebei gateway.",
     types: ["anime"], 
     resources: [
         "catalog",
